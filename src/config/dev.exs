1e3c2731f01c374d8a124bfd215964765c617e2f
Robert Mao <zhihong.mao@gmail.com>
2020-11-02 20:53:16 -0800

    make phoenix serve on the destinated port

--- src/config/dev.exs
+++ src/config/dev.exs
@@ -16,7 +16,7 @@ config :demo, Demo.Repo,
 # watchers to your application. For example, we use it
 # with webpack to recompile .js and .css sources.
 config :demo, DemoWeb.Endpoint,
-  http: [port: 4000],
+  http: [port: System.get_env("BLOCKLET_PORT") || System.get_env("PORT") || 4000],
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
